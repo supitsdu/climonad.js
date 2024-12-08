f2c40fba98452c1c532c8b040460d139af20ca1c
Eduardo M. Santos <contact@supitsdu.me>
2024-12-07 22:26:15 -0300

    build: upload initial setup

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite"
+
+export default defineConfig({
+	root: ".",
+	test: {
+		watch: false,
+		name: "cli",
+		environment: "node",
+		include: ["test/**.test.ts"],
+	},
+})
